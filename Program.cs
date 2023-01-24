73d21332e821ffb2b88dab79f90c27ab2156ee8f
orangebirb <69399170+orangebirb@users.noreply.github.com>
2023-01-24 23:25:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumAllocation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
